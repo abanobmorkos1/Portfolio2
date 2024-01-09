31d65d90b3bca7b6444ce3d460d21a43921075f1
abanobmorkos1 <abanobmorkos11@gmail.com>
2024-01-09 03:08:46 -0500

    pie chart

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,11 +2,12 @@ function navBar(){
     return (
         <nav className="nav">
             <ul className="ul_nav">
-                <li className='skills'>Skills</li>
+                <li className='skillsnav'>Skills</li>
                 <li className='projects'>Projects</li>
                 <li className='contacts'>Contacts</li>
             </ul>
         </nav>
+        
     )
 }
 
