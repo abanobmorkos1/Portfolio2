d744c5d20b722713eabfc2ef9c56cef1895405cf
abanobmorkos1 <abanobmorkos11@gmail.com>
2024-01-09 03:35:34 -0500

    css

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,8 +1,33 @@
-function UncontrolledExample() {
-    return <div>
-      
-  </div>
-  ;
+function App() {
+  const data = [
+    { value: 5, label: 'A' },
+    { value: 10, label: 'B' },
+    { value: 15, label: 'C' },
+    { value: 20, label: 'D' },
+  ];
+
+  const size = {
+    width: 400,
+    height: 200,
+  };
+  // return (
+  //   // <PieChart
+  //   //   series={[
+  //   //     {
+  //   //       arcLabel: (item) => `${item.label} (${item.value})`,
+  //   //       arcLabelMinAngle: 45,
+  //   //       data,
+  //   //     },
+  //   //   ]}
+  //   //   sx={{
+  //   //     [`& .${pieArcLabelClasses.root}`]: {
+  //   //       fill: 'white',
+  //   //       fontWeight: 'bold',
+  //   //     },
+  //   //   }}
+  //   //   {...size}
+  //   // />
+  // );
 }
 
-export default UncontrolledExample;
+export default App;
