d6fb71afa94b34663f6a3ab7913cf1423df0928f
abanobmorkos1 <abanobmorkos11@gmail.com>
2024-01-09 03:43:47 -0500

    donee

--- components/nav.js
+++ components/nav.js
@@ -2,7 +2,7 @@ function navBar(){
     return (
         <nav className="nav">
             <ul className="ul_nav">
-                <li className='skills'>Skills</li>
+                <li className='skillsnav'>Skills</li>
                 <li className='projects'>Projects</li>
                 <li className='contacts'>Contacts</li>
             </ul>
